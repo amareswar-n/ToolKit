ed007c9ae5cd9de4d946c79fefa79efccc6c51d5
AmAr <amareswar.n@gmail.com>
2022-02-01 21:39:28 +0530

    Update PyParseYml_Sql.py

--- python/PyParseYml_Sql.py
+++ python/PyParseYml_Sql.py
@@ -10,31 +10,34 @@ from yaml.loader import SafeLoader
 
 def ParseSql(filePath):
     #filePath = "/Volumes/MyDrive/src/main/resources/sql/hl/hl_ongoing.sql"
-    filename = os.path.basename(filePath)
-    
-    sql_file = open(filePath, "r")
-    sql = sql_file.read()
-    sql_file.close()
-    
-    formatted_sql = sqlparse.format(sql, strip_comments=True, strip_ws=True,  indent_columns= True, reindent_aligned = True, keyword_case='upper')
-    formatted_sql = sqlparse.format(formatted_sql, comma_first = True, reindent = True,)
-    
-    TblNames = list([] )
-    
-    for line in formatted_sql.splitlines():
-      txt = line.strip().split()
-      if len(txt) > 1 and ('FROM' in txt or 'JOIN' in txt):
-          try:
-              if "FROM" in txt and txt.index("FROM")+1 < len(txt):
-                 TblNames.append([filename, txt[txt.index("FROM")+1].upper()])
-                 #print(txt[txt.index("FROM")+1].upper())
-              elif "JOIN" in txt and txt.index("JOIN")+1 < len(txt):
-                 TblNames.append([filename, txt[txt.index("JOIN")+1].upper()])
-                 #print(txt[txt.index("JOIN")+1].upper())
-          except Exception as e:
-              print(e)
-    #print ( TblNames)
-    return TblNames
+    if os.path.exists(filePath):    
+       # filename = os.path.basename(filePath)
+        sql_file = open(filePath, "r")
+        
+        sql = sql_file.read()
+        sql_file.close()
+        
+        formatted_sql = sqlparse.format(sql, strip_comments=True, strip_ws=True,  indent_columns= True, reindent_aligned = True, keyword_case='upper')
+        formatted_sql = sqlparse.format(formatted_sql, comma_first = True, reindent = True,)
+        
+        TblNames = list([] )
+        
+        for line in formatted_sql.splitlines():
+          txt = line.strip().split()
+          if len(txt) > 1 and ('FROM' in txt or 'JOIN' in txt):
+              try:
+                  if "FROM" in txt and txt.index("FROM")+1 < len(txt):
+                     #TblNames.append([filename, txt[txt.index("FROM")+1].upper()])
+                     TblNames.append([txt[txt.index("FROM")+1].upper()])
+                     #print(txt[txt.index("FROM")+1].upper())
+                  elif "JOIN" in txt and txt.index("JOIN")+1 < len(txt):
+                     #TblNames.append([filename, txt[txt.index("JOIN")+1].upper()])
+                     TblNames.append([ txt[txt.index("JOIN")+1].upper()])
+                     #print(txt[txt.index("JOIN")+1].upper())
+              except Exception as e:
+                  print(e)
+        #print ( TblNames)
+        return TblNames
               
 
 
@@ -59,26 +62,33 @@ def loop_path(path):
     #print(" ".join(TableNames))
     print ( TableNames)
     
-def getparams(task, dataframe):
-    nowdf = ("ETL task", "Data Frame", "Table Name", "Filter", "URI", "Persist","","")
+def getparams(task, dataframe, basePath):
+    nowdf = ("ETL task", "Data Frame", "Table Name", "Filter", "Persist","URI", "References","")
     for key in dataframe:
-        table = Filter= uri = persist = ""
+        table = Filter= uri = persist = references = ""
         if key == "table":
             table = dataframe[key]
         elif key == "filter":
             Filter = dataframe[key]
         elif key == "uri":
-            uri = dataframe[key]
+            uri = basePath + dataframe[key]
+            #print(uri)
         elif key == "persist":
             persist = dataframe[key]
         #hiveTable
         #partitionBy
-        
-        nowdf = (task, dataframe["name"], table, Filter, uri, persist )
+        if task == "transform.sql": 
+            references = ParseSql(uri)
+           # print(uri)
+           # print(str(references))
+        nowdf = (task, dataframe["name"], table, Filter, persist, uri, str(references) )
     return (nowdf)            
         
             
 YmlFile = "/Volumes/MyDrive/src/main/resources/hl_al_ongoing.yaml"
+basePath = os.path.dirname(YmlFile)
+#print(basePath)
+
 dataframes= []
 stream = open(YmlFile, 'r')
 pipeline = yaml.load(stream, Loader=SafeLoader)
@@ -90,17 +100,7 @@ for etl in pipeline:
             #print(" ETL TASK " ) #+ str(value ) )
             for inp, dfs in value.items():
                 for dataframe in dfs: 
-                   thisdf = getparams(key, dataframe)
+                   thisdf = getparams(key, dataframe, basePath)
                    dataframes.append(thisdf)
                     #print(dataframe["name"]+ "|" + dataframe["table"] )# + "|" + dataframe["filter"])
 print(dataframes)
-        
-# else:
-          # print(" OTHER TASK")
-
-
-#YmlPath = "hl_al_ongoing.yaml"
-#path="/Volumes/MyDrive/src/main/resources/sql/hl/"
-#loop_path(path)
-        #for token in pipeline:
-            #print(token)
